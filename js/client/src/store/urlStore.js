3e5c7e5ecd7594d88bf70fa2de356ea74c1e6040
Ayush <ayushjadaun6@gmail.com>
2025-03-19 00:05:30 +0530

    deployment

--- js/client/src/store/urlStore.js
+++ js/client/src/store/urlStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/url";
+const API_BASE_URL = "https://urlshortners-1fte.onrender.com/api/url";
 
 const useUrlStore = create((set, get) => ({
   longUrl: "",
